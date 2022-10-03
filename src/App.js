5b7546ad6ad87f7879102e1508e080dacef2ff18
ard work <ardwork17@gmail.com>
2022-10-03 21:00:42 +0700

    commit pertama

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("https://api.github.com/users/ardwork17/repos")
+    .then((response) => {
+      setData(response.data);
+      console.log(response.data)
+  });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container container-repo">
+        <h5 className="title">List Repository</h5>
+      <table class="table table-bordered table-responsive">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">ID</th>
+      <th scope="col">Name Repository</th>
+      <th scope="col">Link Repository</th>
+    </tr>
+  </thead>
+  <tbody>
+  {data.map((repo,index) => (
+    <tr key={index}>
+      <td>{index}</td>
+      <td>{repo.id}</td>
+      <td>{repo.name}</td>
+      <td><a href={repo.html_url} target="_blank">{repo.html_url}</a></td>
+    </tr>
+        ))}
+  </tbody>
+</table>
+      </div>
     </div>
   );
 }
